4019a2acbd1e2689931201cf1efb3cff0dbf1046
SafeSquare <ezeafulukwefredrickosita@gmail.com>
2022-06-16 20:44:09 +0100

    Update

--- js/script.js
+++ js/script.js
@@ -1,134 +1,152 @@
-let postWrapper = document.querySelector('#post-holder');
-let postForm = document.querySelector('#post-form')
-let title = document.querySelector('#title')
-let body = document.querySelector('#body')
+let postWrapper = document.querySelector("#post-holder");
+let postForm = document.querySelector("#post-form");
+let title = document.querySelector("#title");
+let body = document.querySelector("#body");
 
 let postBox = [];
 
 function getPosts() {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-        .then((response) => response.json())
-        .then((data) => {
-            postBox = data;
-            console.log(postBox)
-            let postHolder = '';
-            postBox.forEach(post => {
-                postHolder += `
-                    <div class="col-md-6 mb-3">
-                        <div class="card h-100">
-                            <div class="card-body">
-                                <h6 id="post-title">${post.title}</h6>
-                                <p id="post-body">${post.body}</p>
-                                <div class="d-flex justify-content-between">
-                                    <button class="btn btn-warning">Update</button>
-                                    <button class="btn btn-primary"><a href="post.html" class="text-light" style="text-decoration: none">View</a></</button>
-                                    <button class="btn btn-danger">Delete</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                `
-            });
-        postWrapper.innerHTML = postHolder;
-        })        
+  fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(postBox);
+      //    console.log(data)
+      postBox = data;
+      renderUI(postBox);
+    });
 }
 getPosts();
 
-postForm.addEventListener('submit', createPost)
+postForm.addEventListener("submit", createPost);
 
 function createPost(e) {
-    e.preventDefault();
-    fetch('https://jsonplaceholder.typicode.com/posts', {
-        method: 'POST',
-        body: JSON.stringify({
-            title: title.value,
-            body: body.value,
-            userId: 2
-        }),
-        headers: {
-            'Content-type': 'application/json',
-        },
-    })
+  e.preventDefault();
+  // console.log(title.value, body.value)
+  fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    body: JSON.stringify({
+      title: title.value,
+      body: body.value,
+      userId: 2,
+    }),
+    headers: {
+      "Content-type": "application/json",
+    },
+  })
     .then((response) => response.json())
-    .then((data)=> {
-        console.log(data)
-        postBox.unshift(data);
-        console.log(postBox)
-        let postHolder = '';
-        postBox.forEach(post => {
-            postHolder += `
-                <div class="col-md-6 mb-3">
+    .then((data) => {
+      console.log(data);
+      postBox.unshift(data);
+      console.log(postBox);
+      let postHolder = "";
+      postBox.forEach((post) => {
+        postHolder += `
+                <div class="col-md-4 mb-3">
                     <div class="card h-100">
                         <div class="card-body">
-                            <h6 id="post-title">${post.title}</h6>
-                            <p id="post-body">${post.body}</p>
+                            <h6 class="post-title">${post.title}</h6>
+                            <p class="post-body">${post.body}</p>
                             <div class="d-flex justify-content-between">
-                                <button class="btn btn-warning">Update</button>
-                                <button class="btn btn-primary"><a href="post.html" class="text-light" style="text-decoration: none">View</a></</button>
-                                <button class="btn btn-danger">Delete</button>
+                            <button class="btn btn-warning" id="view-btn" onclick="openSingle(${post.id})">view</button>
+                                <button class="btn btn-primary" onclick="updatePost(${post.id})">Update</button>
+                                <button class="btn btn-danger" onclick="deletePost(${post.id})">Delete</button>
                             </div>
                         </div>
                     </div>
                 </div>
-            `
-        });
-        postWrapper.innerHTML = postHolder;
-    })
+            `;
+      });
+      postWrapper.innerHTML = postHolder;
+    });
 }
 createPost();
 
-function updatePost() {
-    fetch('https://jsonplaceholder.typicode.com/posts/1', {
-        method: 'PUT',
-        body: JSON.stringify({
-            id: 1,
-            title: 'foo',
-            body: 'bar',
-            userId: 1,
-        }),
-        headers: {
-            'Content-type': 'application/json; charset=UTF-8',
-        },
-    })
-        .then((response) => response.json())
-        .then((json) => console.log(json));
+function updatePost(id) {
+  console.log(id);
+
+  fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+    method: "PUT",
+    body: JSON.stringify({
+      id: id,
+      title: title.value,
+      body: body.value,
+      userId: 1,
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      let postTitles = document.querySelectorAll(".post-title"); // 100 post titles [0 -99]
+      let postBodies = document.querySelectorAll(".post-body");
+      console.log(postTitles);
+      postTitles.forEach((postTitle, index) => {
+        if (index + 1 === id) {
+          if (data.title !== "") {
+            postTitle.innerHTML = data.title;
+          }
+        }
+      });
+
+      postBodies.forEach((postBody, index) => {
+        if (index + 1 === id) {
+          if (data.body !== "") {
+            postBody.innerHTML = data.body;
+          }
+        }
+      });
+    });
 }
+updatePost();
 
+function openSingle(id) {
+  fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      localStorage.setItem("viewedPost", JSON.stringify(data));
+      window.location.href = "post.html";
+      // console.log(data)
+    });
+}
+openSingle(id);
 
-// function deletePost(postId) {
-//     console.log(postId)
-//     fetch(`https://jsonplaceholder.typicode.com/posts/${ postId }`, {
-//         method: 'DELETE',
-//   })
-//   .then((response) => response.json())
-//   .then((data)=> {
-//     console.log(data)
-//     console.log(postBox)
-//     postBox.splice((postId - 1), 1)
-    //    console.log(postBox) 
-    //    console.log('post', data) 
-    //    let postHolder = '';
-    //    postBox.forEach(post => {
-    //        postHolder += `
-    //            <div class="col-md-6 mb-3">
-    //                <div class="card h-100">
-    //                    <div class="card-body">
-    //                        <h6 id="post-title">${post.title}</h6>
-    //                        <p id="post-body">${post.body}</p>
-    //                        <div class="d-flex justify-content-between">
-    //                            <button class="btn btn-warning">Update</button>
-    //                            <button class="btn btn-primary"><a href="post.html" class="text-light" style="text-decoration: none">View</a></</button>
-    //                            <button class="btn btn-danger">Delete</button>
-    //                        </div>
-    //                    </div>
-    //                </div>
-    //            </div>
-    //        `
-    //    });
-    //    postWrapper.innerHTML = postHolder;
+function deletePost(id) {
+  fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+    method: "DELETE",
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      postBox = postBox.filter((post) => post.id !== id);
+      console.log(postBox);
+      // use a function to display the UI
+      renderUI(postBox);
+    });
+}
+deletePost(id);
 
-//   }
-//   deletePost();
-// }
-
-
+function renderUI(arr) {
+  let postHolder = "";
+  arr.forEach((post) => {
+    postHolder += `
+                    <div class="col-md-6 mb-3">
+                        <div class="card h-100">
+                            <div class="card-body">
+                                <h6 class="post-title">${post.title}</h6>
+                                <p class="post-body">${post.body}</p>
+                                <div class="d-flex justify-content-between">
+                                    <button class="btn btn-warning" id="view-btn" onclick="openSingle(${post.id})">update</button>
+                                    <button class="btn btn-primary"><a href="post.html" class="text-light" style="text-decoration: none">View</a></</button>
+                                    <button class="btn btn-danger" onclick="deletePost(${post.id})">Delete</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                `;
+  });
+  postWrapper.innerHTML = postHolder;
+}
+renderUI(arr);
