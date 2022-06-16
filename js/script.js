66ecf51a0c8760a1fca87a0f44345fe64e2ddf76
SafeSquare <ezeafulukwefredrickosita@gmail.com>
2022-06-16 11:13:22 +0100

    Update

--- js/script.js
+++ js/script.js
@@ -77,21 +77,58 @@ function createPost(e) {
 }
 createPost();
 
-// function View() {
-//     window.location.href = "post.html";
-// }
+function updatePost() {
+    fetch('https://jsonplaceholder.typicode.com/posts/1', {
+        method: 'PUT',
+        body: JSON.stringify({
+            id: 1,
+            title: 'foo',
+            body: 'bar',
+            userId: 1,
+        }),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    })
+        .then((response) => response.json())
+        .then((json) => console.log(json));
+}
 
-// postForm.addEventListener('submit', deletePost)
 
-// function deletePost() {
-//     fetch('https://jsonplaceholder.typicode.com/posts/1', {
-//     method: 'DELETE',
-//     headers: {
-//         'Content-type': 'application/json'
-//     }
+// function deletePost(postId) {
+//     console.log(postId)
+//     fetch(`https://jsonplaceholder.typicode.com/posts/${ postId }`, {
+//         method: 'DELETE',
 //   })
+//   .then((response) => response.json())
+//   .then((data)=> {
+//     console.log(data)
+//     console.log(postBox)
+//     postBox.splice((postId - 1), 1)
+    //    console.log(postBox) 
+    //    console.log('post', data) 
+    //    let postHolder = '';
+    //    postBox.forEach(post => {
+    //        postHolder += `
+    //            <div class="col-md-6 mb-3">
+    //                <div class="card h-100">
+    //                    <div class="card-body">
+    //                        <h6 id="post-title">${post.title}</h6>
+    //                        <p id="post-body">${post.body}</p>
+    //                        <div class="d-flex justify-content-between">
+    //                            <button class="btn btn-warning">Update</button>
+    //                            <button class="btn btn-primary"><a href="post.html" class="text-light" style="text-decoration: none">View</a></</button>
+    //                            <button class="btn btn-danger">Delete</button>
+    //                        </div>
+    //                    </div>
+    //                </div>
+    //            </div>
+    //        `
+    //    });
+    //    postWrapper.innerHTML = postHolder;
+
 //   }
 //   deletePost();
-
+// }
 
 
